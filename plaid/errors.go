db5bcf5889591307cf9ca4e98ff373ab7057aa14
Your Name <tyamell@gmail.com>
2018-01-04 19:08:20 -0800

    fix error messages

--- plaid/errors.go
+++ plaid/errors.go
@@ -5,16 +5,15 @@ import (
 )
 
 type plaidError struct {
-	// List of all errors: https://github.com/plaid/support/blob/master/errors.md
-	ErrorCode int    `json:"code"`
-	Message   string `json:"message"`
-	Resolve   string `json:"resolve"`
+	ErrorCode string `json:"error_code"`
+	Message   string `json:"error_message"`
+	ErrorType string `json:"error_type"`
 
 	// StatusCode needs to manually set from the http response
 	StatusCode int
 }
 
 func (e plaidError) Error() string {
-	return fmt.Sprintf("Plaid Error - http status: %d, code: %d, message: %s, resolve: %s",
-		e.StatusCode, e.ErrorCode, e.Message, e.Resolve)
+	return fmt.Sprintf("Plaid Error - http status: %d, code: %d, message: %s, type: %s",
+		e.StatusCode, e.ErrorCode, e.Message, e.ErrorType)
 }
