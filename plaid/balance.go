859dc0f06e492cb31c6fb8e3ae3bdb42fbecc064
Your Name <tyamell@gmail.com>
2018-01-04 16:21:35 -0800

    get balances

--- plaid/balance.go
+++ plaid/balance.go
@@ -4,6 +4,31 @@ package plaid
 // 	"bytes"
 // 	"encoding/json"
 // )
+// GetTransactions returns the accounts and transactions associated with the provided
+// access token. Transactions are provided within the range of Start Date and End Date, by default
+// count is 250 and offset is 0.
+func (c *Client) GetBalance(accessToken string) (res *GetBalanceResponse, err error) {
+	request := getTransactionsRequest{
+		AccessToken: accessToken,
+		AccountIds:  nil,
+	}
+
+	var response GetBalanceResponse
+	err = c.post("/accounts/balance/get ", request, &response)
+	return &response, err
+}
+
+type getBalanceRequest struct {
+	AccessToken string   `json:"access_token"`
+	AccountIds  []string `json:"account_ids"`
+}
+
+// GetTransactionsResponse represents the response from GetTransactions
+type GetBalanceResponse struct {
+	Accounts  []Account `json:"accounts"`
+	Item      Item      `json:"item"`
+	RequestID string    `json:"request_id"`
+}
 
 // // Balance (POST /balance) retrieves real-time balance for a given access token.
 // //
